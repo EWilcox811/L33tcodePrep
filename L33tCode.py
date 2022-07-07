86ec874331eedaf72a1db90158f82f8a993db981
Evan Wilcox <ewilcox811@gmail.com>
2022-07-07 15:12:16 -0700

    reverseWords O(n) time complexity based on length of the sentence being reversed.

--- L33tCode.py
+++ L33tCode.py
@@ -11,3 +11,48 @@ def rotate(nums : list, k : int)->None:
 
 nums = [1,2,3,4,5,6,7,8]
 rotate(nums,3)
+
+# Move the zeroes in an integer array to the end of the array in place
+def moveZeroes(nums:list)->None:
+    previous, current = 0, 1
+
+    while previous < len(nums) and current < len(nums):
+        if nums[previous] == 0 and nums[current]!=0:
+            nums[previous], nums[current] = nums[current],nums[previous]
+            previous += 1
+        elif nums[previous]!=0:
+            previous+=1
+        current+=1
+nums = [0,1,0,3,12]
+moveZeroes(nums)
+nums
+
+# find two numbers in an integer array that add up to the target and return the indicies as a base 1
+def twoSum(numbers:list, target:int):
+    low, high = 0, len(numbers)-1
+    while low < high:
+        num = numbers[low]+numbers[high]
+        if num == target:
+            return [low+1, high+1]
+        elif num < target:
+            low+=1
+        else:
+            high -=1
+    return[-1,-1] # This is returned if solution is not found.
+twoSum([2,7,11,15], 9)
+
+# Reverse the letters of each word in a string sentence while preserving the whitespace and word order
+def reverseWords(s):
+    words = [word for word in s.split()]
+    words
+    sentence = ''
+    i = 0
+    while i < len(words):
+        if i != len(words)-1:
+            sentence += words[i][::-1] + ' '
+            i+=1
+        else:
+            sentence += words[i][::-1]
+            i+=1
+    return sentence
+reverseWords("Let's take LeetCode contest")
